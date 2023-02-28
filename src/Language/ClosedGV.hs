e503f8a7c67d18bf358b3a98cc62ca25a4762730
Janek Spaderna <janek@spaderna.eu>
2023-02-28 17:31:10 +0100

    Format some files

--- src/Language/ClosedGV.hs
+++ src/Language/ClosedGV.hs
@@ -1,31 +1,30 @@
-{-# LANGUAGE
-  ConstraintKinds,
-  DataKinds,
-  FlexibleContexts,
-  FunctionalDependencies,
-  GADTs,
-  KindSignatures,
-  NoMonomorphismRestriction,
-  RankNTypes,
-  TypeFamilies,
-  TypeOperators,
-  TypeSynonymInstances
- #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
 
 module Language.ClosedGV where
 
-import Prelude hiding ((^), (<*>), (+))
-
-import Language.LLC
 import Data.Proxy
+import Language.LLC
+import Prelude hiding ((+), (<*>), (^))
 
 -- session types
-data Sess = forall (t :: *).Output t Sess
-          | EndOut
-          | forall (t :: *).Input t Sess
-          | EndIn
-          | Choose Sess Sess
-          | Offer Sess Sess
+data Sess
+  = forall (t :: *). Output t Sess
+  | EndOut
+  | forall (t :: *). Input t Sess
+  | EndIn
+  | Choose Sess Sess
+  | Offer Sess Sess
+
 -- data Output t s
 -- data EndOut
 -- data Input t s
@@ -35,8 +34,11 @@ data Sess = forall (t :: *).Output t Sess
 
 -- infix aliases
 type t <!> s = Output t s
+
 type t <?> s = Input t s
+
 type s1 <++> s2 = Choose s1 s2
+
 type s1 <&&> s2 = Offer s1 s2
 
 -- default wrapper for session types
@@ -51,21 +53,27 @@ data ST (s :: Sess) where
 class (Dual (Dual s) ~ s, Flip (Pol s) ~ Pol (Dual s)) => Session (s :: Sess) where
   polarity :: SPolarity s
   sing :: ST s
+
 instance Session s => Session (t <!> s) where
   polarity = SO
   sing = SOutput Proxy sing
+
 instance Session EndOut where
   polarity = SO
   sing = SEndOut
+
 instance Session s => Session (t <?> s) where
   polarity = SI
   sing = SInput Proxy sing
+
 instance Session EndIn where
   polarity = SI
   sing = SEndIn
+
 instance (Session s1, Session s2) => Session (s1 <++> s2) where
   polarity = SO
   sing = SChoose sing sing
+
 instance (Session s1, Session s2) => Session (s1 <&&> s2) where
   polarity = SI
   sing = SOffer sing sing
@@ -74,11 +82,17 @@ instance (Session s1, Session s2) => Session (s1 <&&> s2) where
 data Polarity = O | I
 
 type family Pol (s :: Sess) :: Polarity
+
 type instance Pol (t <!> s) = O
+
 type instance Pol EndOut = O
+
 type instance Pol (t <?> s) = I
+
 type instance Pol EndIn = I
+
 type instance Pol (s1 <++> s2) = O
+
 type instance Pol (s1 <&&> s2) = I
 
 data SPolarity (s :: Sess) where
@@ -91,48 +105,68 @@ type family Flip (p :: Polarity) :: Polarity where
 
 -- duality
 type family Dual (s :: Sess) :: Sess
-type instance Dual (t <!> s)    = t <?> Dual s
-type instance Dual EndOut       = EndIn
-type instance Dual (t <?> s)    = t <!> Dual s
-type instance Dual EndIn        = EndOut
+
+type instance Dual (t <!> s) = t <?> Dual s
+
+type instance Dual EndOut = EndIn
+
+type instance Dual (t <?> s) = t <!> Dual s
+
+type instance Dual EndIn = EndOut
+
 type instance Dual (s1 <++> s2) = Dual s1 <&&> Dual s2
+
 type instance Dual (s1 <&&> s2) = Dual s1 <++> Dual s2
 
 type DualSession (s :: Sess) = (Session s, Session (Dual s))
 
 class GV (st :: Sess -> *) (repr :: Bool -> [Maybe Nat] -> [Maybe Nat] -> * -> *) | repr -> st where
   send :: DualSession s => repr tf i h t -> repr tf h o (st (t <!> s)) -> repr tf i o (st s)
-  recv :: DualSession s => repr tf i o (st (t <?> s)) ->                      repr tf i o (t * st s)
-  wait :: repr tf i o (st EndIn) ->                                       repr tf i o One
-  fork :: (Session s, Session (Dual s)) => repr tf i o (st s -<> st EndOut) ->                repr tf i o (st (Dual s))
-  chooseLeft  :: (DualSession s1, DualSession s2)
-              => repr tf i o (st (s1 <++> s2)) ->                         repr tf i o (st s1)
-  chooseRight :: (DualSession s1, DualSession s2)
-              => repr tf i o (st (s1 <++> s2)) ->                         repr tf i o (st s2)
-  offer       :: (DualSession s1, DualSession s2)
-              => repr tf i h (st (s1 <&&> s2)) ->
-                   repr tf h o (st s1 -<> t) ->
-                     repr tf h o (st s2 -<> t) ->                         repr tf i o t
+  recv :: DualSession s => repr tf i o (st (t <?> s)) -> repr tf i o (t (*) st s)
+  wait :: repr tf i o (st EndIn) -> repr tf i o One
+  fork :: (Session s, Session (Dual s)) => repr tf i o (st s -<> st EndOut) -> repr tf i o (st (Dual s))
+  chooseLeft ::
+    (DualSession s1, DualSession s2) =>
+    repr tf i o (st (s1 <++> s2)) ->
+    repr tf i o (st s1)
+  chooseRight ::
+    (DualSession s1, DualSession s2) =>
+    repr tf i o (st (s1 <++> s2)) ->
+    repr tf i o (st s2)
+  offer ::
+    (DualSession s1, DualSession s2) =>
+    repr tf i h (st (s1 <&&> s2)) ->
+    repr tf h o (st s1 -<> t) ->
+    repr tf h o (st s2 -<> t) ->
+    repr tf i o t
 
 -- we can encode choice
-chooseLeft'
-  :: (LLC repr, GV st repr, DualSession s1, DualSession s2)
-     => repr False i i (st ((st s1 + st s2) <!> EndOut) -<> st (Dual s1))
+chooseLeft' ::
+  (LLC repr, GV st repr, DualSession s1, DualSession s2) =>
+  repr False i i (st ((st s1 + st s2) <!> EndOut) -<> st (Dual s1))
 chooseLeft' = llam (\m -> fork (llam (\x -> send (inl x) m)))
 
 type DefnGV st tf a =
-    forall repr i vid v v'
-    . (LLC repr, GV st repr, MrgLs i)
-    => repr tf i i a
+  forall repr i vid v v'.
+  (LLC repr, GV st repr, MrgLs i) =>
+  repr tf i i a
+
 defnGV :: DefnGV st tf a -> DefnGV st tf a
 defnGV x = x
 
 bind e f = f ^ e
+
 ret e = e
 
 easiest =
-    defnGV $ fork (llam $ \c -> send (bang (constant 6)) c) `bind` (llam $ \c ->
-             recv c                                         `bind` (llp $  \x c ->
-             wait c                                         `bind` (llz $
-             ret x
-    )))
+  defnGV $
+    fork (llam $ \c -> send (bang (constant 6)) c)
+      `bind` ( llam $ \c ->
+                 recv c
+                   `bind` ( llp $ \x c ->
+                              wait c
+                                `bind` ( llz $
+                                           ret x
+                                       )
+                          )
+             )
--- src/Language/LLC.hs
+++ src/Language/LLC.hs
@@ -1,190 +1,204 @@
-{-# LANGUAGE
-  ConstraintKinds,
-  DataKinds,
-  FlexibleContexts,
-  FlexibleInstances,
-  FunctionalDependencies,
-  MultiParamTypeClasses,
-  RankNTypes,
-  TypeFamilies,
-  TypeOperators,
-  UndecidableInstances
- #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 -- Based on Jeff Polakow, "Embedding a Full Linear Lambda Calculus in Haskell"
 
 module Language.LLC where
 
-import Prelude hiding((^), (<*>), (+))
 import Eq
+import Prelude hiding ((+), (<*>), (^))
 
 --
 -- Linear type constructors
 --
 data a -<> b
+
 data a ->> b
+
 data Bang a
+
 data Top
+
 data a & b
+
 data One
+
 data a * b
+
 data a + b
+
 data Zero
+
 data Base a
 
 infixr 5 -<>
+
 infixr 5 ->>
 
 --
 -- linear variable v in Haskell context
 --
-type LVar repr (x::Nat) a =
-    forall (v::Nat)
-           (i::[Maybe Nat])
-           (o::[Maybe Nat])
-    . (Consume x i o, v ~ Length i) => repr False i o a
+type LVar repr (x :: Nat) a =
+  forall
+    (v :: Nat)
+    (i :: [Maybe Nat])
+    (o :: [Maybe Nat]).
+  (Consume x i o, v ~ Length i) =>
+  repr False i o a
 
 --
 -- unrestricted variable in Haskell context
 --
 type UVar repr a =
-    forall (i::[Maybe Nat])
-    . repr False i i a
+  forall (i :: [Maybe Nat]).
+  repr False i i a
 
 --
 -- The syntax of LLC.
 --
-class LLC (repr :: Bool
-                -> [Maybe Nat]
-                -> [Maybe Nat]
-                -> *
-                -> *
-               ) where
-  llam
-    :: (VarOk tf x, v ~ Length i)
-    => (LVar repr v a -> repr tf
-                              (Just v ': i)
-                              (x ': o)
-                              b
-       )
-    -> repr tf i o (a -<> b)
-  (^)
-    :: repr tf1 i h (a -<> b)
-    -> repr tf2 h o a
-    -> repr (Or tf1 tf2) i o b
+class LLC (repr :: Bool -> [Maybe Nat] -> [Maybe Nat] -> * -> *) where
+  llam ::
+    (VarOk tf x, v ~ Length i) =>
+    ( LVar repr v a ->
+      repr
+        tf
+        (Just v ': i)
+        (x ': o)
+        b
+    ) ->
+    repr tf i o (a -<> b)
+  (^) ::
+    repr tf1 i h (a -<> b) ->
+    repr tf2 h o a ->
+    repr (Or tf1 tf2) i o b
 
-  bang
-    :: repr tf i i a
-    -> repr False i i (Bang a)
-  letBang
-    :: repr tf0 i h (Bang a)
-    -> (UVar repr a -> repr tf1 h o b)
-    -> repr (Or tf0 tf1) i o b
+  bang ::
+    repr tf i i a ->
+    repr False i i (Bang a)
+  letBang ::
+    repr tf0 i h (Bang a) ->
+    (UVar repr a -> repr tf1 h o b) ->
+    repr (Or tf0 tf1) i o b
 
-  ulam
-    :: (UVar repr a -> repr tf i o b)
-    -> repr tf i o (a ->> b)
-  ($$)
-    :: repr tf0 i o (a ->> b)
-    -> repr tf1 o o a
-    -> repr tf0 i o b
+  ulam ::
+    (UVar repr a -> repr tf i o b) ->
+    repr tf i o (a ->> b)
+  ($$) ::
+    repr tf0 i o (a ->> b) ->
+    repr tf1 o o a ->
+    repr tf0 i o b
 
-  top
-    :: repr True i i Top
+  top ::
+    repr True i i Top
 
-  (&)
-    :: ( MrgL h0 tf0 h1 tf1 o
-       , And tf0 tf1 ~ tf
-       )
-    => repr tf0 i h0 a
-    -> repr tf1 i h1 b
-    -> repr tf i o (a & b)
-  pi1
-    :: repr tf i o (a & b)
-    -> repr tf i o a
-  pi2
-    :: repr tf i o (a & b)
-    -> repr tf i o b
+  (&) ::
+    ( MrgL h0 tf0 h1 tf1 o,
+      And tf0 tf1 ~ tf
+    ) =>
+    repr tf0 i h0 a ->
+    repr tf1 i h1 b ->
+    repr tf i o (a & b)
+  pi1 ::
+    repr tf i o (a & b) ->
+    repr tf i o a
+  pi2 ::
+    repr tf i o (a & b) ->
+    repr tf i o b
 
-  one
-    :: repr False i i One
-  letOne
-    :: repr tf0 i h One
-    -> repr tf1 h o a
-    -> repr (Or tf0 tf1) i o a
+  one ::
+    repr False i i One
+  letOne ::
+    repr tf0 i h One ->
+    repr tf1 h o a ->
+    repr (Or tf0 tf1) i o a
 
-  (<*>)
-    :: repr tf0 i h a
-    -> repr tf1 h o b
-    -> repr (Or tf0 tf1) i o (a * b)
-  letStar
-    :: ( VarOk tf1 x
-       , VarOk tf1 y
-       , v ~ Length i
-       )
-    => repr tf0 i h (a * b)
-    -> (LVar repr v a
-        -> LVar repr (S v) b
-        -> repr tf1
-                (Just v ': Just (S v) ': h)
-                (     x ':          y ': o)
-                c
-       )
-    -> repr (Or tf0 tf1) i o c
+  (<*>) ::
+    repr tf0 i h a ->
+    repr tf1 h o b ->
+    repr (Or tf0 tf1) i o (a (*) b)
+  letStar ::
+    ( VarOk tf1 x,
+      VarOk tf1 y,
+      v ~ Length i
+    ) =>
+    repr tf0 i h (a (*) b) ->
+    ( LVar repr v a ->
+      LVar repr (S v) b ->
+      repr
+        tf1
+        (Just v ': Just (S v) ': h)
+        (x ': y ': o)
+        c
+    ) ->
+    repr (Or tf0 tf1) i o c
 
-  inl
-    :: repr tf i o a
-    -> repr tf i o (a + b)
-  inr
-    :: repr tf i o b
-    -> repr tf i o (a + b)
-  letPlus
-    :: ( MrgL o1 tf1 o2 tf2 o
-       , VarOk tf1 x1
-       , VarOk tf2 x2
-       , v ~ Length i
-       )
-    => repr tf0 i h (a + b)
-    -> (LVar repr v a -> repr tf1
-                                (Just v ': h)
-                                (x1 ': o1)
-                                c
-       )
-    -> (LVar repr v b -> repr tf2
-                                (Just v ': h)
-                                (x2 ': o2)
-                                c
-       )
-    -> repr (Or tf0 (And tf1 tf2)) i o c
+  inl ::
+    repr tf i o a ->
+    repr tf i o (a + b)
+  inr ::
+    repr tf i o b ->
+    repr tf i o (a + b)
+  letPlus ::
+    ( MrgL o1 tf1 o2 tf2 o,
+      VarOk tf1 x1,
+      VarOk tf2 x2,
+      v ~ Length i
+    ) =>
+    repr tf0 i h (a + b) ->
+    ( LVar repr v a ->
+      repr
+        tf1
+        (Just v ': h)
+        (x1 ': o1)
+        c
+    ) ->
+    ( LVar repr v b ->
+      repr
+        tf2
+        (Just v ': h)
+        (x2 ': o2)
+        c
+    ) ->
+    repr (Or tf0 (And tf1 tf2)) i o c
 
-  abort
-    :: repr tf i o Zero
-    -> repr True i o a
+  abort ::
+    repr tf i o Zero ->
+    repr True i o a
 
   constant :: a -> repr False i i (Base a)
 
-  ($$$) :: repr tf i h (Base (a -> b))
-        -> repr tf h o (Base a)
-        -> repr tf i o (Base b)
+  ($$$) ::
+    repr tf i h (Base (a -> b)) ->
+    repr tf h o (Base a) ->
+    repr tf i o (Base b)
 
 --
 -- A definition for a closed LLC term.
 --
-type MrgLs i = ( MrgL i False i False i
-               , MrgL i False i True i
-               , MrgL i True i False i
-               , MrgL i True i True i
-               )
+type MrgLs i =
+  ( MrgL i False i False i,
+    MrgL i False i True i,
+    MrgL i True i False i,
+    MrgL i True i True i
+  )
 
---type MrgLs' i v v' = ( MrgL i v i v' i )
+-- type MrgLs' i v v' = ( MrgL i v i v' i )
 
 type Defn tf a =
-    forall repr i v v'
-    . (LLC repr, MrgLs i)
-    => repr tf i i a
+  forall repr i v v'.
+  (LLC repr, MrgLs i) =>
+  repr tf i i a
+
 defn :: Defn tf a -> Defn tf a
 defn x = x
-
 
 {------------------------------------------------------
 
@@ -195,70 +209,92 @@ Type level machinery
 data Nat = Z | S Nat
 
 type family Length (xs :: [Maybe Nat]) :: Nat where
-  Length '[]      = Z
+  Length '[] = Z
   Length (x : xs) = S (Length xs)
 
-type family Or (x::Bool) (y::Bool) :: Bool where
-  Or True  y = True
+type family Or (x :: Bool) (y :: Bool) :: Bool where
+  Or True y = True
   Or False y = y
-  Or x True  = True
+  Or x True = True
   Or x False = x
 
-type family And (x::Bool) (y::Bool) :: Bool where
+type family And (x :: Bool) (y :: Bool) :: Bool where
   And False y = False
-  And True  y = y
+  And True y = y
   And x False = False
-  And x True  = x
+  And x True = x
 
 --
 -- Type level machinery for consuming a variable
 -- in a list of variables.
 --
-class Consume (v::Nat)
-              (i::[Maybe Nat])
-              (o::[Maybe Nat])
-      | v i -> o
-class Consume1 (b::Bool)
-               (v::Nat)
-               (x::Nat)
-               (i::[Maybe Nat])
-               (o::[Maybe Nat])
-      | b v x i -> o
+class
+  Consume
+    (v :: Nat)
+    (i :: [Maybe Nat])
+    (o :: [Maybe Nat])
+    | v i -> o
 
-instance (Consume v i o)
-      => Consume v (Nothing ': i) (Nothing ': o)
-instance (EQ v x b, Consume1 b v x i o)
-      => Consume v (Just x ': i) o
+class
+  Consume1
+    (b :: Bool)
+    (v :: Nat)
+    (x :: Nat)
+    (i :: [Maybe Nat])
+    (o :: [Maybe Nat])
+    | b v x i -> o
+
+instance
+  (Consume v i o) =>
+  Consume v (Nothing ': i) (Nothing ': o)
+
+instance
+  (EQ v x b, Consume1 b v x i o) =>
+  Consume v (Just x ': i) o
 
 instance Consume1 True v x i (Nothing ': i)
-instance (Consume v i o)
-      => Consume1 False v x i (Just x ': o)
+
+instance
+  (Consume v i o) =>
+  Consume1 False v x i (Just x ': o)
 
 --
 -- Type level machinery for merging outputs of
 -- additive operations and getting right Top flag.
 --
-class MrgL (h1::[Maybe Nat])
-           (tf1::Bool)
-           (h2::[Maybe Nat])
-           (tf2::Bool)
-           (h::[Maybe Nat])
-  | h1 h2 -> h
+class
+  MrgL
+    (h1 :: [Maybe Nat])
+    (tf1 :: Bool)
+    (h2 :: [Maybe Nat])
+    (tf2 :: Bool)
+    (h :: [Maybe Nat])
+    | h1 h2 -> h
+
 instance MrgL '[] v1 '[] v2 '[]
-instance (MrgL h1 v1 h2 v2 h)
-      => MrgL (x ': h1) v1 (x ': h2) v2 (x ': h)
-instance (MrgL h1 True h2 v2 h)
-      => MrgL (Just x ': h1) True (Nothing ': h2) v2 (Nothing ': h)
-instance (MrgL h1 v1 h2 True h)
-      => MrgL (Nothing ': h1) v1 (Just x ': h2) True (Nothing ': h)
+
+instance
+  (MrgL h1 v1 h2 v2 h) =>
+  MrgL (x ': h1) v1 (x ': h2) v2 (x ': h)
+
+instance
+  (MrgL h1 True h2 v2 h) =>
+  MrgL (Just x ': h1) True (Nothing ': h2) v2 (Nothing ': h)
+
+instance
+  (MrgL h1 v1 h2 True h) =>
+  MrgL (Nothing ': h1) v1 (Just x ': h2) True (Nothing ': h)
 
 --
 -- Check, in -<> type rule, that Top flag
 -- was set or hypothesis was consumed.
 --
 class VarOk (tf :: Bool) (v :: Maybe Nat)
+
 instance VarOk True (Just v)
+
 instance VarOk True Nothing
+
 instance VarOk False Nothing
 
 -- GHC 8.0.1 cannot infer this type but GHC 7.10.3 can.
@@ -268,14 +304,22 @@ instance VarOk False Nothing
 --
 -- Lambda-bound variables cannot be polymorphic unless they are
 -- specifically annotated as such - or ImpredicativeTypes is enabled.
-llp :: (VarOk tf x, VarOk tf y, VarOk tf Nothing, LLC repr, v ~ Length i) =>
-     (LVar repr (S v) a -> LVar repr (S (S v)) b ->
-        repr tf (Just (S v) : Just (S (S v)) : Nothing : i)
-                (         x :              y : Nothing : o) c) ->
-          repr tf i (o :: [Maybe Nat]) ((a * b) -<> c)
+llp ::
+  (VarOk tf x, VarOk tf y, VarOk tf Nothing, LLC repr, v ~ Length i) =>
+  ( LVar repr (S v) a ->
+    LVar repr (S (S v)) b ->
+    repr
+      tf
+      (Just (S v) : Just (S (S v)) : Nothing : i)
+      (x : y : Nothing : o)
+      c
+  ) ->
+  repr tf i (o :: [Maybe Nat]) ((a (*) b) -<> c)
 llp f = llam (\p -> letStar p f)
+
 llz f = llam (\z -> letOne z f)
 
-compose :: (LLC repr) =>
-           repr False i i ((b -<> c) -<> (a -<> b) -<> a -<> c)
+compose ::
+  (LLC repr) =>
+  repr False i i ((b -<> c) -<> (a -<> b) -<> a -<> c)
 compose = llam (\g -> llam (\f -> llam (\x -> g ^ (f ^ x))))
